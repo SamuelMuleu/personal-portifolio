ae405a3e7a697422b4addb9c55a7355d4db14ea6
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2025-05-06 10:33:25 -0300

    Update main.tsx

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -53,7 +53,7 @@ export function Main() {
           </Avatar>
         </motion.div>
         <motion.div
-          className="flex flex-col items-center w-full mr-[46rem]   md:mr-[30rem]  lg:mr-[44rem]" //
+          className="flex flex-col items-center w-full mr-[46rem]  sm:mr-0 md:mr-[30rem]  lg:mr-[44rem]" //
           initial={{ opacity: 0, scale: 0.5, y: 20 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.9 }}
